9aa0c6ccb36044c1fc7b2355dbd3f29ae408d1e2
linuxhe <linuxhe@tencent.com>
2019-05-23 11:53:41 +0800

    i18n example

--- i18n/go-i18n/example.go
+++ i18n/go-i18n/example.go
@@ -1,11 +1,62 @@
 package main
- 
-import (
-  "github.com/nicksnyder/go-i18n/v2/i18n"
-  "golang.org/x/text/language"
-)
- 
-func main() {
-bundle := &i18n.Bundle{DefaultLanguage: language.English}
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
+)
+
+var bundle *i18n.Bundle
+var lang string
+var loc *i18n.Localizer
+
+func init() {
+	lang = "zh_cn"
+	bundle = &i18n.Bundle{DefaultLanguage: language.English}
+	loc = i18n.NewLocalizer(bundle, lang)
+	if err := loadTranslateFiles("language/"); err != nil {
+		panic(err)
+	}
+}
+
+func loadTranslateFiles(root string) error {
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if _, err := bundle.LoadMessageFile(path); err != nil {
+			return err
+		}
+		return nil
+	})
+	return nil
+}
+
+func I18nStr(key string, param ...interface{}) string {
+	lc := &i18n.LocalizeConfig{
+		MessageID: key,
+	}
+	if len(param) > 1 {
+		lc.TemplateData = map[string]interface{}{
+			param[0].(string): param[1],
+		}
+	}
+
+	translation, err := loc.Localize(lc)
+	if err != nil {
+		fmt.Println("err=", err)
+		return key
+	}
+	return translation
+}
+
+func main() {
+	key := "hello_world"
+	fmt.Println(lang, I18nStr(key))
+
+	key = "missing_param"
+	fmt.Println(lang, I18nStr(key, "Key", "operator"))
 }
